548656d10b40d76e34b0eafe6f55bacfaf2f46ae
Swertex77 <112178774+Swertex77@users.noreply.github.com>
2024-09-12 15:41:58 +0300

    Commit

--- src/Practice_1/Stock.java
+++ src/Practice_1/Stock.java
@@ -1,4 +1,27 @@
 package Practice_1;
 
 public class Stock {
+    String symbol;
+    String name;
+    double previousClosingPrice;
+    double currentPrice;
+
+    Stock(String symbol, String name,  double previousClosingPrice, double currentPrice) {
+        this.symbol = symbol;
+        this.name = name;
+        this.previousClosingPrice = previousClosingPrice;
+        this.currentPrice = currentPrice;
+    }
+
+    double getChangePercent() {
+        return (currentPrice / previousClosingPrice) / 100;
+    }
+}
+
+class TestStock {
+    public static void main(String[] args) {
+        Stock stock = new Stock("SBER", "ПАО Сбербанк", 281.50, 282.87);
+        System.out.println(stock.getChangePercent());
+
+    }
 }
---
+++ src/Practice_2/Author.java
@@ -0,0 +1,46 @@
+package Practice_2;
+
+public class Author {
+    private String email;
+    private String name;
+    private char gender;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + gender + " " + "at" + " " + email;
+    }
+}
+ class TestAuthor {
+     public static void main(String[] args) {
+         Author author = new Author("Egor", "Egor54@mail.ru", 'M');
+
+         author.getEmail();
+         author.getName();
+         author.getGender();
+         author.setEmail("Egorik81@mail.ru");
+         System.out.println(author.toString());
+     }
+ }
